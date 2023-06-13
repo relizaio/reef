2af2e2af8157eb3571a61858b74dff0445807d7e
Pavel Shukhman <taleodor@gmail.com>
2023-06-13 13:33:23 -0400

    fix: templates table

--- migrations/V1__init_schema.sql
+++ migrations/V1__init_schema.sql
@@ -16,3 +16,12 @@ CREATE TABLE instances (
     status text NOT NULL default 'ACTIVE',
     properties jsonb NULL
 );
+
+CREATE TABLE templates (
+    uuid uuid NOT NULL UNIQUE PRIMARY KEY default gen_random_uuid(),
+    created_date timestamptz NOT NULL default now(),
+    last_updated_date timestamptz NOT NULL default now(),
+    schema_version integer NOT NULL default 0,
+    status text NOT NULL default 'ACTIVE',
+    record_data jsonb NULL
+);
