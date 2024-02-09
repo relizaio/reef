584d4201c2f60373a2ccbddab74150a1b061b5e6
Pavel Shukhman <taleodor@gmail.com>
2024-02-09 14:08:07 -0500

    fix: placeholder for future cron service

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import initialize from './api/initialize'
+import cron from './services/cron'
 
 // add timestamps in front of log messages
 require('console-stamp')(console, 'yyyy-mm-dd HH:MM:ss.l')
 
 initialize.startup()
+
+// cron.autoDestroy.start()
